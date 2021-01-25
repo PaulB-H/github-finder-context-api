f7f6cb3a9e3dbb73820e73a5e761ca189e1c5aa9
PaulB-H <p.bernardhall@gmail.com>
2021-01-25 18:27:53 -0500

    Created alert context folder with context, reducer, and state files

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,15 +1,14 @@
 import React from "react";
-// impt
 import PropTypes from "prop-types";
 import RepoItem from "./RepoItem";
 
 const Repos = ({ repos }) => {
-	return repos.map((repo) => <RepoItem repo={repo} key={repo.id} />);
+  return repos.map((repo) => <RepoItem repo={repo} key={repo.id} />);
 };
 
 // ptar
 Repos.propTypes = {
-	repos: PropTypes.array.isRequired,
+  repos: PropTypes.array.isRequired,
 };
 
 export default Repos;
