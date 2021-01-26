755ec2be07f7bacc7890dc6e1f56f96301c82c84
PaulB-H <p.bernardhall@gmail.com>
2021-01-25 20:03:01 -0500

    Removed comments from Search.js, preserved in personal notes

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,35 +2,24 @@ import React, { useState, useContext } from "react";
 import GithubContext from "../../context/github/githubContext";
 import AlertContext from "../../context/alert/alertContext";
 
-// Added props via destructuring here
 const Search = () => {
   const githubContext = useContext(GithubContext);
   const alertContext = useContext(AlertContext);
 
-  // We pull out what we want to call the piece of state and the method
-  // we use to change the piece of state and then our default value
   const [text, setText] = useState("");
 
-  // not a class anymore so we add const before it
   const onSubmit = (e) => {
     e.preventDefault();
-    // this.state.text => text
     if (text === "") {
-      // this.props.setAlert => setAlert & added to props via destructuring
       alertContext.setAlert("Please enter something", "light");
     } else {
       githubContext.searchUsers(text);
-      // this.setState({ text: "" }); => setText('');
       setText("");
     }
   };
 
-  // We no longer do this.setState, we call setText
-  // we no longer have to pass in an object { [e.target.name]: e.target.value }
-  // Just need the value we want to set it to
   const onChange = (e) => setText(e.target.value);
 
-  // No more render we need a return
   return (
     <div>
       <form className="form" onSubmit={onSubmit}>
@@ -38,7 +27,6 @@ const Search = () => {
           type="text"
           name="text"
           placeholder="Search Users..."
-          // this.state.text/onChange => text, onChange
           value={text}
           onChange={onChange}
         />
